fe6dfcffc7e5f4ee32b7850f22ee5b7631af534c
tigerjoy <ranajoy123saha@gmail.com>
2025-09-14 20:41:04 +0530

    Added fix since TASK_INDEX is 0-indexed

--- worker.js
+++ worker.js
@@ -2,5 +2,6 @@
 const TOTAL_TASK_COUNT = process.env.CLOUD_RUN_TASK_COUNT || "10";
 const TASK_INDEX = process.env.CLOUD_RUN_TASK_INDEX || "0";
 
-console.log(`Task ${TASK_INDEX} of ${TOTAL_TASK_COUNT} is complete.`);
+// TASK_INDEX is 0-indexed
+console.log(`Task ${TASK_INDEX + 1} of ${TOTAL_TASK_COUNT} is complete.`);
 process.exit(0);
